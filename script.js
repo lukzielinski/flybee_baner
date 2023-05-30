dd358691e9db6fba86f7368b68873cbd59e2a2d9
ReQ <mrzielony22@gmail.com>
2023-05-31 01:15:52 +0200

    Added animations

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const logoContainer = document.querySelector('.flybee-logo-container');
 const logo = document.querySelector('.flybee-logo');
 const squaresContainer = document.querySelector('.squares-container');
+const destinationContainer = document.querySelector('.destination-container');
+const country = document.querySelector('.country');
 
 logoContainer.classList.add('animate__animated', 'animate__fadeInUpBig');
 logo.classList.add('animate__animated', 'animate__fadeInLeftBig');
@@ -17,3 +19,8 @@ setTimeout(() => {
     squaresContainer.classList.add('animate__animated', 'animate__fadeInUpBig');
     squaresContainer.style.display = 'flex';
 }, 2000);
+
+setTimeout(() => {
+    country.classList.add('country-active');
+    destinationContainer.style.display = 'grid';
+}, 3000);
